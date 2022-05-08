bd733d2e413e559c7402c5583f616915922ca2ee
JuManji <opghld@gmail.com>
2022-05-08 17:33:06 +0300

    Update install-update.sh

--- install-update.sh
+++ install-update.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-apt install unattended-upgrades -y
+apt install unattended-upgrades cron -y
+systemctl enable cron
+systemctl start cron
 echo "45 21 * * * root apt-get update && apt-get -y -d upgrade" | tee -a /etc/crontab
+systemctl status cron
 cat /etc/crontab
