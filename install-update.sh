c154835ac91ed421d17d9f3895e418051ff5bfe3
JuManji <opghld@gmail.com>
2022-05-13 22:42:22 +0300

    Update install-update.sh

--- install-update.sh
+++ install-update.sh
@@ -2,7 +2,7 @@
 apt install unattended-upgrades cron -y
 systemctl enable cron
 systemctl start cron
-echo "50 19 * * 3 root /usr/bin/apt update -q -y" | tee -a /etc/crontab
-echo "0 20 * * 3 root /usr/bin/apt upgrade -q -y" | tee -a /etc/crontab
-echo "0 1 * * * root systemctl reboot -i" | tee -a /etc/crontab
+echo "0 */4 * * * root /usr/bin/apt update -q -y" | tee -a /etc/crontab
+echo "0 */4 * * * root /usr/bin/apt upgrade -q -y" | tee -a /etc/crontab
+echo "0 0 * * * root systemctl reboot -i" | tee -a /etc/crontab
 systemctl restart cron
